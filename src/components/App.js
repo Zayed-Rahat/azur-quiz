856f33aa6f3e90a87143201b2d81c4c9a33104f5
Zayed-Rahat <azurahat129@gmail.com>
2022-05-16 23:52:11 +0600

    private-public-route

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,8 @@ import Login from "./pages/Login";
 import Quiz from "./pages/Quiz";
 import Result from "./pages/Result";
 import Signup from "./pages/Signup";
-
+/* import PrivateRoute from "./PrivateRoute";
+import PublicRoute from "./PublicRoute"; */
 function App() {
   return (
     <Router>
@@ -19,6 +20,10 @@ function App() {
             <Route exact path="/login" element={<Login />} />
             <Route exact path="/quiz" element={<Quiz />} />
             <Route exact path="/result" element={<Result />} />
+            {/*    <PublicRoute exact path="/signup" component={Signup} />
+            <PublicRoute exact path="/login" component={Login} />
+            <PrivateRoute exact path="/quiz" component={Quiz} />
+            <PrivateRoute exact path="/result" component={Result} /> */}
           </Routes>
         </Layout>
       </AuthProvider>
